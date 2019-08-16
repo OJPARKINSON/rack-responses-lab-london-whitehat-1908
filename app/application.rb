dacc70a9f4c07b94399529bdc732df0772999145
Oliver Parkinson <olliepar01@gmail.com>
2019-08-16 09:20:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
     
     t = Time.hour
     if t <= 12 then
-    resp.write "Good Morning"
+      resp.write "Good Morning"
     else 
-    resp.write "Good Afternoon"
-  end
+      resp.write "Good Afternoon"
+    end
     resp.finished
   end
 end 
