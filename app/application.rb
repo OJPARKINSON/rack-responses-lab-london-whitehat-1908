bd0bc0ad308b07180a7795ad2b8cc56acb1269eb
Oliver Parkinson <olliepar01@gmail.com>
2019-08-16 09:16:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    t = Time.now
+    t = Time.hour.now
     resp.write "Good Morning"
     
     resp. write "Good Afternoon"
