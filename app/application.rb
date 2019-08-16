782e24505a6bb98d4f309b160265918df66c715e
Oliver Parkinson <olliepar01@gmail.com>
2019-08-16 09:20:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,11 @@ class Application
     resp = Rack::Response.new
     
     t = Time.hour
+    if t <= 12 then
     resp.write "Good Morning"
-    
+    else 
     resp.write "Good Afternoon"
-    
+  end
     resp.finished
   end
 end 
